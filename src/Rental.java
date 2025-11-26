aff5502f3b2e5ba99c0c6bfe8e1798d9a4132e83
paulakrupinski <paulakrupinski@gmail.com>
2025-11-26 20:54:43 -0300

    Commit 9

--- src/Rental.java
+++ src/Rental.java
@@ -1,4 +1,5 @@
-class Rental {
+public class Rental {
+
     private Movie movie;
     private int daysRented;
 
@@ -15,36 +16,14 @@ class Rental {
         return daysRented;
     }
 
-    // Novo método extraído para entregar pontos de fidelidade
-    public int getFrequentRenterPoints() {
-        // Regra: filmes NEW_RELEASE rendem 2 pontos se alugados por mais de 1 dia
-        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
-        return 1;
+    
+    public double getCharge() {
+        return movie.getCharge(daysRented);
     }
 
-    public double getCharge() {
-        double thisAmount = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) {
-                    thisAmount += (daysRented - 2) * 1.5;
-                }
-                break;
-
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3) {
-                    thisAmount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+    public int getFrequentRenterPoints() {
+        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+            return 2;
+        return 1;
     }
 }
