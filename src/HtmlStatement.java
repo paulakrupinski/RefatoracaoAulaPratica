7ec9cdcd7d4d8c50a40dfd74aa109e41cb9d5777
paulakrupinski <paulakrupinski@gmail.com>
2025-11-26 22:02:42 -0300

    Commit 18

--- src/HtmlStatement.java
+++ src/HtmlStatement.java
@@ -1,20 +1,22 @@
 public class HtmlStatement extends Statement {
 
     @Override
-    protected String header(Customer aCustomer) {
-        return "<h1>Rental Record for <em>" + aCustomer.getName() + "</em></h1><p>\n";
+    protected String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
     }
 
     @Override
-    protected String eachRental(Rental rental) {
-        return rental.getMovie().getTitle() + ": " + rental.getCharge() + "<br>\n";
+    protected String eachRentalString(Rental aRental) {
+        return aRental.getMovie().getTitle() + ": " +
+               String.valueOf(aRental.getCharge()) + "<BR>\n";
     }
 
     @Override
-    protected String footer(Customer aCustomer) {
-        return "<p>You owe <em>" + aCustomer.getTotalCharge() + "</em><br>\n" +
-               "On this rental you earned <em>" +
+    protected String footerString(Customer aCustomer) {
+        return "<P>You owe <EM>" + aCustomer.getTotalCharge() +
+               "</EM><P>\n" +
+               "On this rental you earned <EM>" +
                aCustomer.getTotalFrequentRenterPoints() +
-               "</em> frequent renter points<p>\n";
+               "</EM> frequent renter points<P>";
     }
 }
